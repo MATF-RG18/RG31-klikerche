2ff12fdcfc7b385abeb63ffb8cf639dd41e91734
matfija <lazar1997@mts.rs>
2018-11-15 07:27:42 +0100

    Modularizacija u okviru main-a, kasnije treba podeliti po fajlovima

--- src/main.c
+++ src/main.c
@@ -11,70 +11,87 @@
 #define PERSP_BLIZ 1
 #define PERSP_DALJ 10
 
+// Preciznost sfere
+#define SFERA_PREC 22
+
 // Aski kod eskejp karaktera
 #define ESC 27
 
-// Makro za nepotrebne promenljive
+// Makro za nekoriscene promenljive
 #define PONISTI(x) (void)x
 
-// Parametri sfernih koordinata oka/kamere:
-// poluprecnik r, azimutni ugao phi, polarni
-// ugao theta i promene prilikom kretanja
-static float r = 5, phi = 0, theta = M_PI/6,
-             d_r = 0.15, d_phi = M_PI/90, d_theta = M_PI/90;
+// Struktura koja enkapsulira oko/kameru,
+// predstavljeno sfernim koordinatama
+static struct oko{
+    // Poluprecnik i promena
+    GLdouble r, d_r;
+    
+    // Azimutni ugao i promena
+    GLdouble phi, d_phi;
+    
+    // Polarni ugao i promena
+    GLdouble theta, d_theta;
+    
+    // Dekartove koordinate
+    GLdouble x, y, z;
+} oko;
 
-// Deklaracija kolbek funkcija
-static void na_tipku(unsigned char tipka, int x, int y);
-static void na_prozor(int sirina, int visina);
+// Struktura koja enkapsulira klikerche
+static struct kliker{
+    // Koordinate centra
+    GLdouble x, y, z;
+    
+    // Poluprecnik
+    GLdouble r;
+    
+    // Preciznost
+    GLdouble prec;
+} kliker;
+
+// Deklaracije fja za oko/kameru
+static void napravi_oko(void);
+static void popravi_oko(void);
+static void oko_levo(void);
+static void oko_desno(void);
+static void oko_gore(void);
+static void oko_dole(void);
+static void oko_napred(void);
+static void oko_nazad(void);
+
+// Deklaracije fja za kliker
+static void napravi_kliker(void);
+static void nacrtaj_kliker(void);
+
+// Deklaracije kolbek funkcija
+static void na_tipku(unsigned char, int, int);
+static void na_prozor(int, int);
 static void na_prikaz(void);
+
+// Deklaracije sablonskih funkcija
+static void podesi_biblioteke(int, char**);
+static void napravi_prozor(void);
+static void postavi_kolbek(void);
+static void postavi_svetlo(void);
 
 int main(int arg_br, char** arg_niz)
 {
-    // Inicijalizacija Gluta
-    glutInit(&arg_br, arg_niz);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+    // Pokretanje Gluta
+    podesi_biblioteke(arg_br, arg_niz);
     
     // Pravljenje prozora
-    glutInitWindowSize(PROZ_DIM, PROZ_DIM);
-    glutInitWindowPosition(PROZ_POL, PROZ_POL);
-    glutCreateWindow(APP_IME);
+    napravi_prozor();
     
-    // Ucitavanje kolbek funkcija
-    glutKeyboardFunc(na_tipku);
-    glutReshapeFunc(na_prozor);
-    glutDisplayFunc(na_prikaz);
+    // Postavljanje kolbek funkcija
+    postavi_kolbek();
     
-    // Postavljanje OpenGL parametara
-    glClearColor(0.05, 0.05, 0.05, 0);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_LIGHTING);
+    // Postavljanje svetla i materijala
+    postavi_svetlo();
     
-    // Ambijentalna, difuzna i spekularna
-    // boja svetla koje osvetljuje scenu
-    GLfloat amb_svetlo[] = {0.1, 0.1, 0.1, 1};
-    GLfloat dif_svetlo[] = {0.7, 0.7, 0.7, 1};
-    GLfloat spek_svetlo[] = {0.9, 0.9, 0.9, 1};
+    // Pravljenje oka/kamere
+    napravi_oko();
     
-    // Postavljanje svojstava svetla
-    glEnable(GL_LIGHT0);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, amb_svetlo);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, dif_svetlo);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, spek_svetlo);
-    
-    // Ambijentalna, difuzna i spekularna
-    // refleksija materijala objekata na sceni
-    GLfloat amb_ref[] = {0.3, 0.7, 0.3, 1};
-    GLfloat dif_ref[] = {0.2, 1, 0.2, 1};
-    GLfloat spek_ref[] = {1, 1, 1, 1};
-    
-    // Glatkost materijala
-    GLfloat glatkost = 100;
-    
-    // Postavljanje svojstava materijala
-    glMaterialfv(GL_FRONT, GL_AMBIENT, amb_ref);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, dif_ref);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, spek_ref);
-    glMaterialf(GL_FRONT, GL_SHININESS, glatkost);
+    // Pravljenje klikercheta
+    napravi_kliker();
     
     // Glavna petlja Gluta
     glutMainLoop();
@@ -84,6 +101,7 @@ int main(int arg_br, char** arg_niz)
 
 static void na_tipku(unsigned char tipka, int x, int y)
 {
+    // Zanemaruje se mesto klika
     PONISTI(x);
     PONISTI(y);
     
@@ -94,14 +112,7 @@ static void na_tipku(unsigned char tipka, int x, int y)
     
     case 'a':
     case 'A':
-        // Oko se krece levo:
-        // smanjivanje azimuta
-        phi -= d_phi;
-        
-        // Popravka jer phi = [0, 2pi)
-        if (phi < 0){
-            phi += 2*M_PI;
-        }
+        oko_levo();
         
         // Osvezavanje scene
         glutPostRedisplay();
@@ -109,14 +120,7 @@ static void na_tipku(unsigned char tipka, int x, int y)
     
     case 'd':
     case 'D':
-        // Oko se krece desno:
-        // povecavanje azimuta
-        phi += d_phi;
-        
-        // Popravka jer phi = [0, 2pi)
-        if (phi >= 2*M_PI){
-            phi -= 2*M_PI;
-        }
+        oko_desno();
         
         // Osvezavanje scene
         glutPostRedisplay();
@@ -124,14 +128,7 @@ static void na_tipku(unsigned char tipka, int x, int y)
     
     case 'e':
     case 'E':
-        // Oko se priblizava:
-        // smanjivanje radijusa
-        r -= d_r;
-        
-        // Popravka jer r = [2, 10]
-        if (r < 2){
-            r = 2;
-        }
+        oko_napred();
         
         // Osvezavanje scene
         glutPostRedisplay();
@@ -139,14 +136,7 @@ static void na_tipku(unsigned char tipka, int x, int y)
     
     case 'q':
     case 'Q':
-        // Oko se udaljava:
-        // povecavanje radijusa
-        r += d_r;
-        
-        // Popravka jer r = [2, 10]
-        if (r > 10){
-            r = 10;
-        }
+        oko_nazad();
         
         // Osvezavanje scene
         glutPostRedisplay();
@@ -155,23 +145,15 @@ static void na_tipku(unsigned char tipka, int x, int y)
     case 'r':
     case 'R':
         // Resetovanje oka/kamere
-        phi = 0;
-        theta = M_PI/6;
+        napravi_oko();
+        
+        // Osvezavanje scene
         glutPostRedisplay();
         break;
     
     case 's':
     case 'S':
-        // Oko se krece dole:
-        // smanjivanje polara
-        theta -= d_theta;
-        
-        // Popravka jer theta = [0, pi/2]
-        // Inace je [-pi/2, pi/2], ali ovako
-        // se izbegava odlazak ispod scene
-        if (theta < 0){
-            theta = 0;
-        }
+        oko_dole();
         
         // Osvezavanje scene
         glutPostRedisplay();
@@ -179,14 +161,7 @@ static void na_tipku(unsigned char tipka, int x, int y)
     
     case 'w':
     case 'W':
-        // Oko se krece gore:
-        // povecavanje polara
-        theta += d_theta;
-        
-        // Popravka jer theta = [0, pi/2]
-        if (theta > M_PI/2){
-            theta = M_PI/2;
-        }
+        oko_gore();
         
         // Osvezavanje scene
         glutPostRedisplay();
@@ -221,16 +196,187 @@ static void na_prikaz(void)
     GLfloat svetlo_pol[] = {0, 0, 0, 1};
     glLightfv(GL_LIGHT0, GL_POSITION, svetlo_pol);
     
-    // Postavljanje vidnih parametara u skladu sa
-    // matematickom formulacijom sfernih koordinata
-    gluLookAt(r * cos(theta) * cos(phi),
-              r * cos(theta) * sin(phi),
-              r * sin(theta),
-              0, 0, 0, 0, 0, 1);
+    // Postavljanje vidnih parametara
+    popravi_oko();
+    gluLookAt( oko.x,    oko.y,    oko.z,   // centar oka/kamere
+              kliker.x, kliker.y, kliker.z, // centar pogleda
+                 0,        0,        1);    // vektor normale
     
-    // Pravljenje kugle tj. klikercheta
-    glutWireSphere(1, 20, 20);
+    // Crtanje kugle tj. klikercheta
+    nacrtaj_kliker();
     
     // Zamena bafera tj. prikaz slike na ekranu
     glutSwapBuffers();
+}
+
+static void podesi_biblioteke(int arg_br, char** arg_niz)
+{
+    // Pokretanje Gluta
+    glutInit(&arg_br, arg_niz);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+}
+
+static void napravi_prozor(void)
+{
+    // Pravljenje prozora
+    glutInitWindowSize(PROZ_DIM, PROZ_DIM);
+    glutInitWindowPosition(PROZ_POL, PROZ_POL);
+    glutCreateWindow(APP_IME);
+    
+    // OpenGL ciscenje prozora
+    glClearColor(0.05, 0.05, 0.05, 0);
+}
+
+static void postavi_kolbek(void)
+{
+    // Postavljanje kolbek funkcija
+    glutKeyboardFunc(na_tipku);
+    glutReshapeFunc(na_prozor);
+    glutDisplayFunc(na_prikaz);
+}
+
+static void postavi_svetlo(void)
+{
+    // Ukljucivanje dubine
+    glEnable(GL_DEPTH_TEST);
+    
+    // Ukljucivanje svetla
+    glEnable(GL_LIGHTING);
+    
+    // Ambijentalna, difuzna i spekularna
+    // boja svetla koje osvetljuje scenu
+    GLfloat amb_svetlo[] = {0.1, 0.1, 0.1, 1};
+    GLfloat dif_svetlo[] = {0.7, 0.7, 0.7, 1};
+    GLfloat spek_svetlo[] = {0.9, 0.9, 0.9, 1};
+    
+    // Postavljanje svojstava svetla
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, amb_svetlo);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, dif_svetlo);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, spek_svetlo);
+    
+    // Ambijentalna, difuzna i spekularna
+    // refleksija materijala objekata na sceni
+    GLfloat amb_ref[] = {0.3, 0.7, 0.3, 1};
+    GLfloat dif_ref[] = {0.2, 1, 0.2, 1};
+    GLfloat spek_ref[] = {1, 1, 1, 1};
+    
+    // Glatkost materijala
+    GLfloat glatkost = 100;
+    
+    // Postavljanje svojstava materijala
+    glMaterialfv(GL_FRONT, GL_AMBIENT, amb_ref);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, dif_ref);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, spek_ref);
+    glMaterialf(GL_FRONT, GL_SHININESS, glatkost);
+}
+
+static void napravi_oko(void)
+{
+    oko.r = 5;
+    oko.d_r = 0.15;
+    
+    oko.phi = 0;
+    oko.d_phi = M_PI/90;
+    
+    oko.theta = M_PI/6;
+    oko.d_theta = M_PI/90;
+}
+
+static void popravi_oko(void)
+{
+    oko.x = oko.r * cos(oko.theta) * cos(oko.phi);
+    oko.y = oko.r * cos(oko.theta) * sin(oko.phi);
+    oko.z = oko.r * sin(oko.theta);
+}
+
+static void oko_levo(void)
+{
+    // Oko se krece levo:
+    // smanjivanje azimuta
+    oko.phi -= oko.d_phi;
+    
+    // Popravka jer phi = [0, 2pi)
+    if (oko.phi < 0){
+        oko.phi += 2*M_PI;
+    }
+}
+
+static void oko_desno(void)
+{
+    // Oko se krece desno:
+    // povecavanje azimuta
+    oko.phi += oko.d_phi;
+    
+    // Popravka jer phi = [0, 2pi)
+    if (oko.phi >= 2*M_PI){
+        oko.phi -= 2*M_PI;
+    }
+}
+
+static void oko_gore(void)
+{
+    // Oko se krece gore:
+    // povecavanje polara
+    oko.theta += oko.d_theta;
+    
+    // Popravka jer theta = [0, pi/2]
+    if (oko.theta > M_PI/2){
+        oko.theta = M_PI/2;
+    }
+}
+
+static void oko_dole(void)
+{
+    // Oko se krece dole:
+    // smanjivanje polara
+    oko.theta -= oko.d_theta;
+    
+    // Popravka jer theta = [0, pi/2]
+    // Inace je [-pi/2, pi/2], ali ovako
+    // se izbegava odlazak ispod scene
+    if (oko.theta < 0){
+        oko.theta = 0;
+    }
+}
+
+static void oko_napred(void)
+{
+    // Oko se priblizava:
+    // smanjivanje radijusa
+    oko.r -= oko.d_r;
+    
+    // Popravka jer r = [2, 10]
+    if (oko.r < 2){
+        oko.r = 2;
+    }
+}
+
+static void oko_nazad(void)
+{
+    // Oko se udaljava:
+    // povecavanje radijusa
+    oko.r += oko.d_r;
+    
+    // Popravka jer r = [2, 10]
+    if (oko.r > 10){
+        oko.r = 10;
+    }
+}
+
+
+static void napravi_kliker(void)
+{
+    kliker.x = 0;
+    kliker.y = 0;
+    kliker.z = 0;
+    
+    kliker.r = 1;
+    
+    kliker.prec = SFERA_PREC;
+}
+
+static void nacrtaj_kliker(void)
+{
+    glutWireSphere(kliker.r, kliker.prec, kliker.prec);
 }
